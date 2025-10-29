ac3b49df209fb76ebf68975865ad57b38352486f
chanhong-ji <jichanhong24@gmail.com>
2025-10-29 13:12:07 +0900

    prev

--- app/features/expenses/pages/expenses-page.tsx
+++ app/features/expenses/pages/expenses-page.tsx
@@ -36,34 +36,36 @@ export default function ExpensesPage({ loaderData }: Route.ComponentProps) {
     <div className='flex flex-col items-center justify-center'>
       <DateCard year={year} month={month} />
       <MonthlyOverview year={year} month={month} />
-      <ItemGroup className='w-3xl mt-10 space-y-4 border-2 border-accent-foreground/2'>
-        <Item variant='outline'>
-          <ItemContent>
-            <ItemDescription>Tuesday, 27th October 2025</ItemDescription>
-            <ItemTitle>Movie Tickets with friends</ItemTitle>
-            <div className='flex w-full flex-wrap gap-2'>
-              <Badge variant='secondary'>Food</Badge>
-            </div>
-          </ItemContent>
-          <ItemContent>
-            <ItemTitle>100000 KRW</ItemTitle>
-          </ItemContent>
-          <ItemActions>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant='ghost' className='h-8 w-8 p-0'>
-                  <span className='sr-only'>Open menu</span>
-                  <MoreHorizontal className='h-4 w-4' />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align='end'>
-                <DropdownMenuItem>Edit</DropdownMenuItem>
-                <DropdownMenuItem>Delete</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </ItemActions>
-        </Item>
-      </ItemGroup>
+      <div className='w-3xl mt-10 space-y-4 border-2 border-accent-foreground/2'>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <Item variant='outline'>
+            <ItemContent>
+              <ItemDescription>Tuesday, 27th October 2025</ItemDescription>
+              <ItemTitle>Movie Tickets with friends</ItemTitle>
+              <div className='flex w-full flex-wrap gap-2'>
+                <Badge variant='secondary'>Food</Badge>
+              </div>
+            </ItemContent>
+            <ItemContent>
+              <ItemTitle>100000 KRW</ItemTitle>
+            </ItemContent>
+            <ItemActions>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant='ghost' className='h-8 w-8 p-0'>
+                    <span className='sr-only'>Open menu</span>
+                    <MoreHorizontal className='h-4 w-4' />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align='end'>
+                  <DropdownMenuItem>Edit</DropdownMenuItem>
+                  <DropdownMenuItem>Delete</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </ItemActions>
+          </Item>
+        ))}
+      </div>
     </div>
   );
 }
