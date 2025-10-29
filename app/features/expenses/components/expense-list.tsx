0c17c6d930a238ffb12b227532995e81b6734157
chanhong-ji <jichanhong24@gmail.com>
2025-10-29 14:19:36 +0900

    feat: 지출 내역 삭제 모달 추가

--- app/features/expenses/components/expense-list.tsx
+++ app/features/expenses/components/expense-list.tsx
@@ -10,6 +10,17 @@ import { Button } from "~/common/components/ui/button";
 import { MoreHorizontal } from "lucide-react";
 import { Dialog, DialogTrigger } from "~/common/components/ui/dialog";
 import { EditExpenseDialog } from "./edit-expense-dialog";
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+} from "~/common/components/ui/alert-dialog";
+import { Form } from "react-router";
 
 export function ExpenseList() {
   return (
@@ -27,23 +38,45 @@ export function ExpenseList() {
             <ItemTitle>100000 KRW</ItemTitle>
           </ItemContent>
           <ItemActions>
-            <Dialog>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="ghost" className="h-8 w-8 p-0">
-                    <span className="sr-only">Open menu</span>
-                    <MoreHorizontal className="h-4 w-4" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DialogTrigger asChild>
-                    <DropdownMenuItem>Edit</DropdownMenuItem>
-                  </DialogTrigger>
-                  <DropdownMenuItem>Delete</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-              <EditExpenseDialog />
-            </Dialog>
+            <AlertDialog>
+              <Dialog>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                      <span className="sr-only">Open menu</span>
+                      <MoreHorizontal className="h-4 w-4" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DialogTrigger asChild>
+                      <DropdownMenuItem>Edit</DropdownMenuItem>
+                    </DialogTrigger>
+                    <AlertDialogTrigger asChild>
+                      <DropdownMenuItem onSelect={(e) => e.preventDefault()}>Delete</DropdownMenuItem>
+                    </AlertDialogTrigger>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <EditExpenseDialog />
+              </Dialog>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>해당 지출을 삭제할까요?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    이 작업은 되돌릴 수 없습니다. 삭제를 진행하시겠습니까?
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>취소</AlertDialogCancel>
+                  <Form method="post" className="inline-flex">
+                    <input type="hidden" name="intent" value="delete-expense" />
+                    <input type="hidden" name="expenseId" value={String(index)} />
+                    <Button type="submit" variant="destructive">
+                      삭제
+                    </Button>
+                  </Form>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           </ItemActions>
         </Item>
       ))}
